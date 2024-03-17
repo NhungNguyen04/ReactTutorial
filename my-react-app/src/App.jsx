772b76966adcec9ea7220e61c10909c969cdba0f
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-03-17 16:39:06 +0700

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,20 @@
-import Button from './Button.jsx'
 
+// props = read-only properties that are shared between components.
+//          A parent component an send data to a child component
+//          <Component key=value/>
+
+
+import Student from './Student.jsx'
 function App() {
   
   return(
-    <Button/>
-  );
+    <div>
+      <Student name="Timothee Chalamet" age={30} isStudent={false}/>
+      <Student name="Harry Potter" age={15} isStudent={true}/>
+      <Student name="Nhung Ne" age={20} isStudent={true}/>
+      <Student/>
+    </div>
+  )
 }
 
 export default App
