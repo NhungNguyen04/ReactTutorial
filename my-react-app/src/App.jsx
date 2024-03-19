eb9014c604ef5b419f8bac8a1cb1ab386aa39322
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-03-19 15:53:56 +0700

    Rendering Lists

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,30 @@
-import UserGreeting from "./UserGreeting.jsx"
+import List from './List.jsx'
 
 function App() {
   
-  return(
-    <>
-      <UserGreeting isLoggedIn="false" username = "Nhung"/>
-    </>
-  )
+  const fruits = [
+    {id: 1, name: "apple", calories: 95},
+    {id: 2, name: "orange", calories: 45},
+    {id: 3, name: "banana", calories: 105},
+    {id: 4, name: "coconut", calories: 159},
+    {id: 5, name: "pineapple", calories: 37}
+   ];
+  
+  const vegetables = [
+    {id: 1, name: "potatoes", calories: 110},
+    {id: 2, name: "celery", calories: 15},
+    {id: "3", name: "carrots", calories: 25},
+    {id: 4, name: "corn", calories: 63},
+    {id: 5, name: "broccoli", calories: 50}
+   ];
+
+  
+
+  return (<>
+  {fruits.length > 0 && <List/>}
+  {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
+  </>);
+
 }
 
 export default App
