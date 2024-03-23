3e296ef511876348b56b8e59aea0750a16b747c8
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-03-23 11:46:55 +0700

    to do list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
-import MyComponent2 from "./MyComponent2.jsx"
+import ToDoList from "./ToDoList.jsx"
 
 function App() {
   
   return (
     <>
-    <MyComponent2/>
+    <ToDoList/>
     </>
   );
   
